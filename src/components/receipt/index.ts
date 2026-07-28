58f6e96da42f0775a3ad2c38ae58048b93789e32
willy-de7 <stacyshima0@gmail.com>
2026-07-28 15:24:58 +0100

    design: add supplier redemption scanner UI (#455)
    Co-authored-by: thlpkee20-wq <thlpkee20@gmail.com>

--- src/components/receipt/index.ts
+++ src/components/receipt/index.ts
@@ -1,5 +1,6 @@
 export * from "./Receipt";
 export * from "./ReceiptModal";
+export * from "./RedemptionScanner";
 export * from "./NotesEditor";
 export * from "./masking";
 export * from "./types";
